76abf3dfc677f72f74d81a58b37e14dc249bf9fd
becem-gharbi <99251251+becem-gharbi@users.noreply.github.com>
2023-03-27 15:59:07 +0100

    fix: add CORS and CSP protection

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,10 +19,32 @@ export default defineNuxtConfig({
     "@nuxtjs/tailwindcss",
     "@bg-dev/nuxt-s3",
     "@nuxt/devtools",
+    "nuxt-security",
   ],
 
   auth,
   naiveui,
   tailwindcss,
   s3,
+
+  security: {
+    corsHandler: {
+      origin: process.env.AUTH_BASE_URL,
+      methods: "*",
+    },
+    xssValidator: false,
+    headers: {
+      crossOriginEmbedderPolicy: false,
+      contentSecurityPolicy: {
+        "img-src": [
+          "'self'",
+          "data:",
+          "blob:",
+          "https://*.googleusercontent.com",
+          process.env.S3_PUBLIC_BUCKET_URL || "",
+        ],
+      },
+    },
+  },
+
 });

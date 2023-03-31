ce7c0911ad960b9e54daa9814f47600266733291
becem-gharbi <99251251+becem-gharbi@users.noreply.github.com>
2023-03-31 15:11:01 +0100

    refactor: change AccountInfo sizes

--- components/Account/Info.vue
+++ components/Account/Info.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="flex items-center gap-3">
-        <S3Image v-if="user" :src="user?.picture" class="w-9 h-9 object-contain rounded-full ring-2" />
+        <S3Image v-if="user" :src="user?.picture" class="w-8 h-8 object-contain rounded-full ring-2" />
 
-        <div class="flex flex-col">
+        <div class="flex flex-col text-base md:text-sm">
             <NText strong>{{ user?.name }}</NText>
             <NText :depth="3">{{ user?.email }}</NText>
         </div>

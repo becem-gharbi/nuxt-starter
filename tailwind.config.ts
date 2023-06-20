0e205ece985c53642e873d6e9f1dd7a7584c70e8
becem gharbi <becem.gharbi@live.com>
2023-06-20 14:09:59 +0100

    refactor(tailwind.config): Use ESM export

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,10 @@
-module.exports = {
+import type { Config } from "tailwindcss";
+
+export default <Partial<Config>>{
   darkMode: "class",
+  theme: {
+    fontFamily: {
+      sans: ["Helvetica"],
+    },
+  },
 };

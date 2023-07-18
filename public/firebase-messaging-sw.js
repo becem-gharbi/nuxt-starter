7d636bd069895b865427f9a36746bda6eb987a37
becem gharbi <becem.gharbi@live.com>
2023-07-18 13:28:42 +0100

    chore: Update firebase config

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -6,13 +6,13 @@ importScripts(
 );
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA-ZLjOO7ln-D7jzewjQg5HzSD-bWLDPdo",
-  authDomain: "oauth-388710.firebaseapp.com",
-  projectId: "oauth-388710",
-  storageBucket: "oauth-388710.appspot.com",
-  messagingSenderId: "608224103831",
-  appId: "1:608224103831:web:75e719c2e93cfcc28608d2",
-  measurementId: "G-EMFKD7JXYN"
+  apiKey: "AIzaSyCkXK7YmLmj06A6W1LnyDPT7TIFdfzQv1s",
+  authDomain: "opensource-393209.firebaseapp.com",
+  projectId: "opensource-393209",
+  storageBucket: "opensource-393209.appspot.com",
+  messagingSenderId: "757120780044",
+  appId: "1:757120780044:web:d9fd0f794cc146ab81ab29",
+  measurementId: "G-ZME8S32QQC"
 };
 
 const app = firebase.initializeApp(firebaseConfig);

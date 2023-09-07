7f5483979158cab0979c326fcada7991c840e0ce
becem gharbi <becem.gharbi@live.com>
2023-09-07 13:09:21 +0100

    refactor(drawer): Display account button on footer

--- components/Navbar/Index.vue
+++ components/Navbar/Index.vue
@@ -1,5 +1,5 @@
 <template>
-    <naive-navbar v-if="user" :routes="routes" drawer-closable>
+    <naive-navbar :routes="routes" drawer-closable>
         <template #start>
             <NuxtLink to="/" class="flex items-center gap-3">
                 <NaiveIcon name="logos:nuxt-icon" :size="25"></NaiveIcon>
@@ -7,11 +7,13 @@
             </NuxtLink>
         </template>
 
-        <template #end v-if="!isMobileOrTablet">
-            <n-dropdown trigger="click" :options="dropdownOptions" :style="{ padding: '8px', minWidth: '200px' }"
-                @select="handleSelect">
-                <img :src="user.picture" class="w-8 h-8 object-cover rounded-full ring-2 cursor-pointer">
-            </n-dropdown>
+        <template #end>
+            <div v-if="!isMobileOrTablet" class="notMobileOrTablet">
+                <n-dropdown trigger="click" :options="dropdownOptions" :style="{ padding: '8px', minWidth: '200px' }"
+                    @select="handleSelect">
+                    <img :src="user?.picture" class="w-8 h-8 object-cover rounded-full ring-2 cursor-pointer" />
+                </n-dropdown>
+            </div>
         </template>
 
         <template #drawer-header>
@@ -19,69 +21,58 @@
         </template>
 
         <template #drawer-footer>
-            <n-button secondary block @click="async () => await logout()">
-                Logout
-            </n-button>
+            <div class="flex flex-col gap-3 w-full">
+                <n-button secondary block @click="() => navigateTo('/account')">
+                    Account
+                </n-button>
+                <n-button secondary block @click="() => logout()">
+                    Logout
+                </n-button>
+            </div>
         </template>
     </naive-navbar>
 </template>
 
 <script setup lang="ts">
-import { NaiveIcon, AccountInfo } from "#components"
-import { NavbarRoute } from "@bg-dev/nuxt-naiveui"
-import type { DropdownOption } from "naive-ui"
+import { NaiveIcon, AccountInfo } from "#components";
+import { NavbarRoute } from "@bg-dev/nuxt-naiveui";
+import type { DropdownOption } from "naive-ui";
 
-const { user } = useAuthSession()
-const { logout } = useAuth()
-const { isMobileOrTablet } = useNaiveDevice()
+const { user } = useAuthSession();
+const { logout } = useAuth();
+const { isMobileOrTablet } = useNaiveDevice();
 
-const routes = ref<NavbarRoute[]>([])
+const routes = ref<NavbarRoute[]>([]);
 
-if (isMobileOrTablet) {
-    routes.value.push({
+const dropdownOptions = ref<DropdownOption[]>([]);
+
+dropdownOptions.value = [
+    {
+        key: "header",
+        type: "render",
+        render: () => h(AccountInfo),
+    },
+    {
+        key: "divider",
+        type: "divider",
+    },
+    {
         label: "Account",
-        path: "/account",
-        icon: 'ph:user'
-    })
-}
-
-const dropdownOptions = ref<DropdownOption[]>([])
-
-watch(user, (newUser, oldUser) => {
-    if (!newUser || newUser.role === oldUser?.role) {
-        return
-    }
-
-    dropdownOptions.value = [
-        {
-            key: 'header',
-            type: 'render',
-            render: () => h(AccountInfo)
-        },
-        {
-            key: 'divider',
-            type: 'divider',
-        },
-        {
-            label: 'Account',
-            key: 'account',
-            icon: () => h(NaiveIcon, { name: 'ph:user' }),
-        },
-        {
-            label: 'Logout',
-            key: 'logout',
-            icon: () => h(NaiveIcon, { name: 'ph:sign-out' }),
-        }
-    ]
-}, { immediate: true })
-
+        key: "account",
+        icon: () => h(NaiveIcon, { name: "ph:user" }),
+    },
+    {
+        label: "Logout",
+        key: "logout",
+        icon: () => h(NaiveIcon, { name: "ph:sign-out" }),
+    },
+];
 
 async function handleSelect(key: string) {
-    if (key === 'logout') {
-        await logout()
-    }
-    else if (key === 'account') {
-        await navigateTo('/account')
+    if (key === "logout") {
+        await logout();
+    } else if (key === "account") {
+        await navigateTo("/account");
     }
 }
 </script>

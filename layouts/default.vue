5d2a6437373427f793417119de3a7089ff346ed6
becem-gharbi <becem.gharbi@live.com>
2023-12-27 19:12:36 +0100

    refactor(ui): Use `naive-layout-navbar` as default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,41 @@
 <template>
-  <div class="page">
-    <Navbar />
+  <naive-layout-navbar :routes="routes" :drawer-routes="drawerRoutes">
+    <template #start>
+      <nuxt-link to="/" class="flex items-center gap-3">
+        <naive-icon name="logos:nuxt-icon" :size="25" />
+        <n-text strong>
+          Nuxt starter
+        </n-text>
+      </nuxt-link>
+    </template>
+
+    <template #end>
+      <div class="notMobileOrTablet">
+        <n-dropdown
+          placement="bottom-start"
+          trigger="click"
+          :options="dropdownOptions"
+          :style="{ padding: '8px', minWidth: '200px' }"
+          @select="handleDropdownSelect"
+        >
+          <img
+            :src="user?.picture"
+            class="w-8 h-8 object-cover rounded-full ring-2 cursor-pointer"
+            alt="avatar"
+          >
+        </n-dropdown>
+      </div>
+    </template>
+
+    <template #drawer-header>
+      <AccountInfo class="mx-2" />
+    </template>
+
+    <template #drawer-footer>
+      <n-button secondary block @click="logout">
+        Logout
+      </n-button>
+    </template>
 
     <ClientOnly>
       <n-notification-provider placement="bottom-right">
@@ -11,6 +46,54 @@
     <div class="flex-1 container mx-auto my-8 px-4">
       <slot />
     </div>
-    <Footer />
-  </div>
+  </naive-layout-navbar>
 </template>
+
+<script setup lang="ts">
+import type { MenuLinkRoute } from '@bg-dev/nuxt-naiveui'
+import type { DropdownOption } from 'naive-ui'
+import { NaiveIcon, AccountInfo } from '#components'
+
+const { user } = useAuthSession()
+const { logout } = useAuth()
+
+const routes: MenuLinkRoute[] = []
+
+const drawerRoutes: MenuLinkRoute[] = [
+  {
+    label: 'Account',
+    icon: 'ph:user',
+    path: '/account'
+  }
+]
+
+const dropdownOptions: DropdownOption[] = [
+  {
+    key: 'header',
+    type: 'render',
+    render: () => h(AccountInfo)
+  },
+  {
+    key: 'divider',
+    type: 'divider'
+  },
+  {
+    label: 'Account',
+    key: 'account',
+    icon: () => h(NaiveIcon, { name: 'ph:user' })
+  },
+  {
+    label: 'Logout',
+    key: 'logout',
+    icon: () => h(NaiveIcon, { name: 'ph:sign-out' })
+  }
+]
+
+async function handleDropdownSelect (key: string) {
+  if (key === 'logout') {
+    await logout()
+  } else if (key === 'account') {
+    await navigateTo('/account')
+  }
+}
+</script>

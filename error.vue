e6dbabbf987e8fdf2ff6d74bdcb6a16abd1ac1e6
becem-gharbi <becem.gharbi@live.com>
2024-02-28 19:33:57 +0100

    fix: use custom naiveui theme on error page

--- error.vue
+++ error.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="page justify-center items-center">
-    <n-result
-      :status="error.statusCode?.toString()"
-      :builtin-theme-overrides="{ titleFontSizeMedium: '24px' }"
-      :title="error.statusMessage || 'Oops, something went wrong'"
-      :description="error.message"
-    >
-      <template #footer>
-        <n-button @click="handleError">
-          Back home
-        </n-button>
-      </template>
-    </n-result>
-  </div>
+  <naive-config>
+    <div class="page justify-center items-center">
+      <n-result
+        :status="error.statusCode?.toString()"
+        :builtin-theme-overrides="{ titleFontSizeMedium: '24px' }"
+        :title="error.statusMessage || 'Oops, something went wrong'"
+        :description="error.message"
+      >
+        <template #footer>
+          <n-button type="primary" @click="handleError">
+            Back home
+          </n-button>
+        </template>
+      </n-result>
+    </div>
+  </naive-config>
 </template>
 
 <script setup lang="ts">
